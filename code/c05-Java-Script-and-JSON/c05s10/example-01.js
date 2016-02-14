c3234c9f7a51c3998744a05bd75dc06638618177
Verrol Adams <verrol@supercool.com>
2016-02-14 12:44:13 -0500

    code example for c05s10 - Containers: Arrays

---
+++ code/c05-Java-Script-and-JSON/c05s10/example-01.js
@@ -0,0 +1,9 @@
+var day1 = [76, 81, 79]
+var day2 = [79, 83, 80]
+var day3 = [80, 85, 80]
+var day4 = [81, 87, 79]
+var day5 = [72, 79, 75]
+var day6 = [75, 79, 72]
+var day7 = [81, 88, 77]
+
+console.log("day 1 temperature readings: ", day1)
---
+++ code/c05-Java-Script-and-JSON/c05s10/example-02.js
@@ -0,0 +1,13 @@
+var day1 = [76, 81, 79]
+var day2 = [79, 83, 80]
+var day3 = [80, 85, 80]
+var day4 = [81, 87, 79]
+var day5 = [72, 79, 75]
+var day6 = [75, 79, 72]
+var day7 = [81, 88, 77]
+
+console.log("day 1 temperature readings: ", day1)
+
+var week1 = [day1, day2, day3, day4, day5, day6, day7]
+
+console.log("week 1 temperature readings: \n", week1);
