bcafd90106d06bed9b9e7fd65a512a0e00627224
Verrol Adams <verrol@supercool.com>
2016-02-15 14:41:48 -0500

    Code example for c05s11 - Arrays : More On Usage

---
+++ code/c05-Java-Script-and-JSON/c05s11/example-01.js
@@ -0,0 +1,18 @@
+// creating arrays
+var day1 = [76, 81, 79]
+
+console.log("day 1 temperature readings: ", day1)
+
+// accessing elements of the array
+var day1MorningTemp = day1[0]
+var day1MiddayTemp = day1[1]
+var day1EveningTemp = day1[2]
+
+console.log("Day 1 8am temperature: " + day1MorningTemp)
+console.log("Day 1 12pm temperature: " + day1MiddayTemp)
+console.log("Day 1 6pm temperature: " + day1EveningTemp)
+
+console.log("\nPrint again...")
+console.log("Day 1 8am temperature: " + day1[0])
+console.log("Day 1 12pm temperature: " + day1[1])
+console.log("Day 1 6pm temperature: " + day1[2])
---
+++ code/c05-Java-Script-and-JSON/c05s11/example-02.js
@@ -0,0 +1,22 @@
+var day1 = [76, 81, 79]
+var day2 = [79, 83, 80]
+var day3 = [80, 85, 80]
+var day4 = [81, 87, 79]
+var day5 = [72, 79, 75]
+var day6 = [75, 79, 72]
+var day7 = [81, 88, 77]
+
+console.log("Number of temperature readings per day: ", day1.length)
+
+var week1 = [day1, day2, day4, day6, day7]
+
+console.log("Number of days reading for week1: ", week1.length);
+
+/*
+ * Output:
+ *------------------
+
+Number of temperature readings per day:  3
+Number of days reading for week1:  5
+
+*/
