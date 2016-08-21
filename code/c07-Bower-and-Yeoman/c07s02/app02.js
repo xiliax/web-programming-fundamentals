6e987a7afb719eabeb47b6813604540bfa97b5cd
Verrol Adams <verrol@supercool.com>
2016-08-21 13:47:52 -0400

    Update docs, slides, and code for c07s02 - #66 - Writing a Simple Web Server in NodeJS

---
+++ code/c07-Bower-and-Yeoman/c07s02/app02.js
@@ -0,0 +1,19 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  console.log("URL: ", req.url)
+  console.log("Method: ", req.method)
+  console.log("Headers: ", req.headers)
+  console.log("")
+
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
